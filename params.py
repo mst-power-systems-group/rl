4bf7f19d183da0ace073993326257f184f437475
ronitdas <ronitdas.2014@gmail.com>
2019-09-16 09:43:33 -0500

    Update params.py

--- params.py
+++ params.py
@@ -7,22 +7,18 @@ action_size = 7
 alpha = 0.55
 gamma = 0.6
 epsilon = 0.2
-hour = 24
 
 #current_states = np.zeros(hour)
 #actions = np.ones(hour)
 #Q_table = np.zeros(shape=(hour, state_size, action_size))
 
-current_states = [0] * 24
-actions = [1] * 24
+current_state = 0
+action = 1
 
 Q_table = []
 
-for i in range(24):
+for i in range(state_size):
   row = []
-  for j in range(state_size):
-    column = []
-    for k in range(action_size):
-      column.append(0.0)
-    row.append(column)
+  for j in range(action_size):
+    row.append(0.0)
   Q_table.append(row)
