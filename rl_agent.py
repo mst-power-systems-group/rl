affeae33d64f670415334d6feb41e58b800bb52d
ronitdas <ronitdas.2014@gmail.com>
2019-09-16 09:40:35 -0500

    Update rl_agent.py

--- rl_agent.py
+++ rl_agent.py
@@ -8,32 +8,29 @@ rand = False
 
 #params.init()
 
-next_states = []
-profits = []
-
 def update_table(hour, curr_state, act, nxt_state, reward):
     
-    params.Q_table[hour][curr_state][act] += params.alpha * (reward + params.gamma * np.amax(params.Q_table[hour][nxt_state]) - params.Q_table[hour][curr_state][act])
+    params.Q_table[hour][curr_state][act] += params.alpha * (reward + params.gamma * max(params.Q_table[hour][nxt_state]) - params.Q_table[hour][curr_state][act])
     
 def get_action(hour, curr):
     
     if (rand == True):
         return np.random.randint(0, params.action_size)
     else:
-        return np.argmax(params.Q_table[hour][curr]) #return the index, NOT THE VALUE!
+        return Q_table.index(max(params.Q_table[hour][curr])) #return the index, NOT THE VALUE!
 
 
 
-def Q_learning(next_states, profits):
+def Q_learning(next_state, profit):
     
     if random() <= params.epsilon:
         rand = True
     
     for i in range(24):
-        update_table(i, params.current_states[i], params.actions[i], next_states[i], profits[i])
-        params.current_states[i] = next_states[i]
-        params.actions[i] = get_action(i, params.current_states[i])
+        update_table(i, params.current_state, params.action, next_state, profit)
+        params.current_state = next_state
+        params.action = get_action(i, params.current_state)
     
     #update_params(day)
         
-    return params.actions
+    return params.action
