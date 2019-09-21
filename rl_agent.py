27ebe333012eb9f9b23d9161938221c3c97d1f7b
ChenHaotianC <49891560+ChenHaotianC@users.noreply.github.com>
2019-09-21 10:11:18 -0500

    Add files via upload
    I Marked where I changed. In my mind, there should be only one q value in the q table changing  in each iteration, but so far all the q value changed

--- rl_agent.py
+++ rl_agent.py
@@ -13,24 +13,37 @@ def update_table(curr_state, act, nxt_state, reward):
     params.Q_table[curr_state][act] += params.alpha * (reward + params.gamma * max(params.Q_table[nxt_state]) - params.Q_table[curr_state][act])
     
 def get_action(curr):
-    
+    global rand              #add
     if (rand == True):
         return np.random.randint(0, params.action_size)
     else:
-        return Q_table.index(max(params.Q_table[curr])) #return the index, NOT THE VALUE!
+        return params.Q_table[curr].index(max(params.Q_table[curr])) #return the index, NOT THE VALUE!
 
 
 
-def Q_learning(next_state, profit):
-    
-    if random() <= params.epsilon:
-        rand = True
+def Q_learning(next_state, profit, Qtable,day):
+    params.Q_table = Qtable      #add
+    #print(params.Q_table,"\n")
+    #if random() <= params.epsilon:
+    if day <= 500:            #add
+        global rand           #add
+        rand = True           #add
     
     for i in range(24):
+        
         update_table(params.current_state, params.action, next_state, profit)
         params.current_state = next_state
         params.action = get_action(params.current_state)
+
+    print(params.Q_table,"\n")
+
+
     
     #update_params(day)
         
     return params.action
+
+
+def getQtable():
+
+    return params.Q_table
