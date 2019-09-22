6b3c70d9aad80027d472171ae2d77ad886c93c9c
ChenHaotianC <49891560+ChenHaotianC@users.noreply.github.com>
2019-09-22 12:24:31 -0500

    Add files via upload

--- rl_agent.py
+++ rl_agent.py
@@ -2,7 +2,7 @@ import params
 import numpy as np
 from random import seed, random
 
-seed(1)
+#seed(1)
 
 rand = False
 
@@ -13,29 +13,36 @@ def update_table(curr_state, act, nxt_state, reward):
     params.Q_table[curr_state][act] += params.alpha * (reward + params.gamma * max(params.Q_table[nxt_state]) - params.Q_table[curr_state][act])
     
 def get_action(curr):
-    global rand              #add
+    print("random is ",rand)
     if (rand == True):
+        print("random action")
         return np.random.randint(0, params.action_size)
     else:
+        print("not random action")
         return params.Q_table[curr].index(max(params.Q_table[curr])) #return the index, NOT THE VALUE!
 
 
 
 def Q_learning(next_state, profit, Qtable,day):
     params.Q_table = Qtable      #add
-    #print(params.Q_table,"\n")
-    #if random() <= params.epsilon:
-    if day <= 500:            #add
-        global rand           #add
+    #print(params.Q_table,"\n"    #if random() <= params.epsilon:
+    global rand           #add
+    if day <= 200:            #add      
         rand = True           #add
+    else:
+        rand = False
     
-    for i in range(24):
-        
-        update_table(params.current_state, params.action, next_state, profit)
-        params.current_state = next_state
-        params.action = get_action(params.current_state)
+    params.current_state = next_state             #add 
+    update_table(params.current_state, params.action, next_state, profit)       #add
+    params.action = get_action(params.current_state)         #add
 
-    print(params.Q_table,"\n")
+    #for i in range(24):       #comment out
+        
+    #    update_table(params.current_state, params.action, next_state, profit)   #comment out
+    #    params.current_state = next_state         #comment out
+    #    params.action = get_action(params.current_state)         #comment out
+
+    #print(params.Q_table,"\n")
 
 
     
@@ -45,5 +52,6 @@ def Q_learning(next_state, profit, Qtable,day):
 
 
 def getQtable():
+    #print(params.Q_table,"\n")
 
     return params.Q_table
