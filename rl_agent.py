31b1b4b3f2347dbcc464bcfddcef87e2693d1297
ronitdas <ronitdas.2014@gmail.com>
2019-09-22 13:30:47 -0500

    Update rl_agent.py

--- rl_agent.py
+++ rl_agent.py
@@ -1,10 +1,9 @@
 import params
-import numpy as np
-from random import seed, random
+from random import seed, random, randint
 
 #seed(1)
 
-rand = False
+#rand = False
 
 #params.init()
 
@@ -13,28 +12,53 @@ def update_table(curr_state, act, nxt_state, reward):
     params.Q_table[curr_state][act] += params.alpha * (reward + params.gamma * max(params.Q_table[nxt_state]) - params.Q_table[curr_state][act])
     
 def get_action(curr):
-    print("random is ",rand)
-    if (rand == True):
+    print("random is ",params.rand)
+    if (params.rand == True):
         print("random action")
-        return np.random.randint(0, params.action_size)
+        return randint(0, params.action_size)
     else:
         print("not random action")
         return params.Q_table[curr].index(max(params.Q_table[curr])) #return the index, NOT THE VALUE!
-
-
-
-def Q_learning(next_state, profit, Qtable,day):
-    params.Q_table = Qtable      #add
-    #print(params.Q_table,"\n"    #if random() <= params.epsilon:
-    global rand           #add
-    if day <= 200:            #add      
-        rand = True           #add
-    else:
-        rand = False
     
+def update_epsilon():
+    
+    if params.epsilon > params.epsilon_min:
+        params.epsilon *= params.epsilon_decay
+    else:
+        params.epsilon = 0.0
+        
+
+def update_alpha():
+    
+    if params.alpha > params.alpha_min:
+        params.alpha *= params.alpha_decay
+
+
+
+#def Q_learning(next_state, profit, Qtable,day):
+def Q_learning(next_state, profit):
+    #params.Q_table = Qtable      #add
+    #print(params.Q_table,"\n"    #if random() <= params.epsilon:
+    #global rand           #add
+    #if day <= 200:            #add      
+       # rand = True           #add
+    #else:
+        #rand = False
+        
+    
+    if (random() <= params.epsilon):
+        params.rand = True
+    else:
+        params.rand = False
+        
+      
     params.current_state = next_state             #add 
     update_table(params.current_state, params.action, next_state, profit)       #add
-    params.action = get_action(params.current_state)         #add
+    params.action = get_action(params.current_state) #add
+    
+    update_epsilon()
+    
+    update_alpha()
 
     #for i in range(24):       #comment out
         
