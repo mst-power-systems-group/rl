9161f07b761b37aced0e46cae2b6d55569fbd65d
ronitdas <ronitdas.2014@gmail.com>
2019-10-31 22:07:56 -0500

    Update params.py

--- params.py
+++ params.py
@@ -1,12 +1,13 @@
 import pprint
 import collections as coll
+import numpy as np
 
 global state_size
 global action_size
 global alpha
 global gamma
 global epsilon
-global rand
+#global rand
 #global Q_table
 global current_state
 global action
@@ -15,33 +16,35 @@ global epsilon_decay
 global alpha_min
 global alpha_decay
 global memory
-global sample_batch_size
+global batch_size
+global agent_current_state
+global action
 
-state_size = 1
+state_size = 12
 action_size = 10
 alpha = 0.5
-gamma = 0.7
-epsilon = 0.2
+gamma = 0.8
+epsilon = 1.0
 epsilon_min = 0.01
 #alpha_min = 0.001
 epsilon_decay = 0.995
 #alpha_decay = 0.99
 memory = coll.deque(maxlen=3000)
-sample_batch_size = 32
+batch_size = 32
 
-rand = False
+action = 1
+
+agent_current_state = [0]*state_size
+
+agent_current_state[0] = 1
+
+agent_current_state = np.array(agent_current_state)
+
+#rand = False
 
 #current_states = np.zeros(hour)
 #actions = np.ones(hour)
 #Q_table = np.zeros(shape=(hour, state_size, action_size))
 
-current_state = 0
-action = 1
 
-'''Q_table = []
 
-for i in range(state_size):
-  row = []
-  for j in range(action_size):
-    row.append(0.0)
-  Q_table.append(row)'''
